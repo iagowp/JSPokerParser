7c2ce167c3f89ebd390028b4fa331cf828c7c8e5
iagowp <iago.wp@hotmail.com>
2015-08-21 15:42:50 -0300

    hand parser now working in english for holden tournaments and cash games

--- parser.js
+++ parser.js
@@ -1,17 +1,14 @@
 var handParser = function(handHistory){
   // object with all the relevant info to be displayed
   var parsedHand = {};
-  parsedHand.platforms = "";
-  parsedHand.gameStyle = "";
+  parsedHand.platforms = parsedHand.gameStyle = parsedHand.language = parsedHand.turn = parsedHand.river = "";
   parsedHand.players = [];
   parsedHand.hands = [];
   parsedHand.preFlopActions = [];
   parsedHand.table = [];
   parsedHand.flop = [];
   parsedHand.flopActions = [];
-  parsedHand.turn = '';
   parsedHand.turnActions = [];
-  parsedHand.river = '';
   parsedHand.riverActions = [];
 
   handHistory = handHistory.split("\n");
@@ -20,90 +17,179 @@ var handParser = function(handHistory){
   // need to prepare for other platforms
   if(metaHand.indexOf('PokerStars') !== -1) parsedHand.platforms = "PS";
   // need prepare for different styles
-  parsedHand.gameStyle = metaHand.indexOf("Hold'em No Limit") != -1 ? "Hold'em No Limit" : false;
+  parsedHand.gameStyle = metaHand.indexOf("Hold'em No Limit") !== -1 ? "Hold'em No Limit" : false;
+  parsedHand.language = handHistory[1].indexOf("Mesa") === 0 ? 'pt' : 'en'
 
   var row = 2;
 
-  // need to create a new function for this
-  while( handHistory[row].indexOf('Lugar ') === 0 ){
-    var endOfNumber = handHistory[row].indexOf(' em fichas');
-    var numIndex = endOfNumber;
-    while( handHistory[row][numIndex] !== '('){
-      numIndex--;
-    }
+  if(parsedHand.language === 'en'){
 
-    var chips = Number(handHistory[row].substring(numIndex +1, endOfNumber))
-
-    parsedHand.players.push(["player " + (parsedHand.players.length+1), chips]);
-    row++;
-  }
-
-  //advance to the hand row
-  while( handHistory[row].indexOf("*** ") !== 0 ){
-    row++;
-  }
-
-  // need to be prepared for hand history where no hand is known
-  var ownHand = handHistory[++row].split('[');
-  ownHand = ownHand[ownHand.length-1].substring(0, 5).split(" ");
-
-  parsedHand.hands.push(ownHand);
-
-  // todo: check if there are other hands available
-
-  //advance to the next event
-  while( handHistory[row].indexOf("*** ") !== 0 ){
-    parsedHand.preFlopActions.push(handHistory[row]);
-    row++;
-  }
-
-  //check if there's a FLOP
-  if(handHistory[row].indexOf("FLOP") !== -1){
-    //get the FLOP cards
-    parsedHand.flop = handHistory[row].split('[');
-    parsedHand.flop = parsedHand.flop[1].substring(0, 8).split(" ");
-    parsedHand.table = parsedHand.flop.slice();
-    row++;
-
-    // advance to the next event
-    while( handHistory[row].indexOf("*** ") !== 0 ){
-      parsedHand.flopActions.push(handHistory[row]);
+    //prevent rebuys from getting in the way
+    while( handHistory[row].indexOf('re-buys and receives') !== -1 ){
       row++;
     }
 
-    // check if there's a TURN
-    if( handHistory[row].indexOf("TURN") === 4){
-      //get the TURN card
-      parsedHand.turn = handHistory[row].split('[');
-      parsedHand.turn = parsedHand.turn[2].substring(0, 2);
-      parsedHand.table.push(parsedHand.turn);
-      row++;
+    while( handHistory[row].indexOf('Seat ') === 0 ){
+      var endOfNumber = handHistory[row].indexOf(' in chips');
+      var numIndex = endOfNumber;
+      while( handHistory[row][numIndex] !== '('){
+        numIndex--;
+      }
+      var chips = handHistory[row].substring(numIndex +1, endOfNumber);
 
+      parsedHand.players.push(["player " + (parsedHand.players.length+1), chips]);
+      row++;
+    }
+
+    //advance to the hand row, needs to save actions
+    while( handHistory[row].indexOf(" ") !== 0 && handHistory[row].indexOf("*** ") !== 0){
+      parsedHand.preFlopActions.push(handHistory[row]);
+      row++;
+    }
+
+    // need to be prepared for hand history where no hand is known
+    var ownHand = handHistory[++row].split('[');
+    ownHand = ownHand[ownHand.length-1].substring(0, 5).split(" ");
+
+    parsedHand.hands.push(ownHand);
+    row++;
+
+    // todo: check if there are other hands available
+
+    //advance to the next event
+    while( handHistory[row].indexOf(" ") !== 0 && handHistory[row].indexOf("*** ") !== 0 ){
+      parsedHand.preFlopActions.push(handHistory[row]);
+      row++;
+    }
+
+    //check if there's a FLOP
+    if(handHistory[row].indexOf("FLOP") !== -1){
+      //get the FLOP cards
+      parsedHand.flop = handHistory[row].split('[');
+      parsedHand.flop = parsedHand.flop[1].substring(0, 8).split(" ");
+      parsedHand.table = parsedHand.flop.slice();
+      row++;
 
       // advance to the next event
-      while( handHistory[row].indexOf("*** ") !== 0 ){
-        parsedHand.turnActions.push(handHistory[row]);
+      while( handHistory[row].indexOf(" ") !== 0 && handHistory[row].indexOf("*** ") !== 0 ){
+        parsedHand.flopActions.push(handHistory[row]);
         row++;
       }
 
-      // check if there's a RIVER
-      if( handHistory[row].indexOf("RIVER") === 4){
-        //get the RIVER card
-        parsedHand.river = handHistory[row].split('[');
-        parsedHand.river = parsedHand.river[2].substring(0, 2);
-        parsedHand.table.push(parsedHand.river);
+      // check if there's a TURN
+      if( handHistory[row].indexOf("TURN") !== -1){
+        //get the TURN card
+        parsedHand.turn = handHistory[row].split('[');
+        parsedHand.turn = parsedHand.turn[2].substring(0, 2);
+        parsedHand.table.push(parsedHand.turn);
         row++;
 
         // advance to the next event
-        while( handHistory[row].indexOf("*** ") !== 0 ){
-          parsedHand.riverActions.push(handHistory[row]);
+        while( handHistory[row].indexOf(" ") !== 0 && handHistory[row].indexOf("*** ") !== 0 ){
+          parsedHand.turnActions.push(handHistory[row]);
           row++;
+        }
+
+        if( handHistory[row].indexOf("RIVER") !== -1){
+          //get the RIVER card
+          parsedHand.river = handHistory[row].split('[');
+          parsedHand.river = parsedHand.river[2].substring(0, 2);
+          parsedHand.table.push(parsedHand.river);
+          row++;
+
+          // advance to the next event
+          while( handHistory[row].indexOf(" ") !== 0 && handHistory[row].indexOf("*** ") !== 0){
+            parsedHand.riverActions.push(handHistory[row]);
+            row++;
+          }
+
+        }
+      }
+    }
+
+  } else {
+    // code for portuguese
+    // need to create a new function for this
+    while( handHistory[row].indexOf('Lugar ') === 0 ){
+      var endOfNumber = handHistory[row].indexOf(' em fichas');
+      var numIndex = endOfNumber;
+      while( handHistory[row][numIndex] !== '('){
+        numIndex--;
+      }
+
+      var chips = Number(handHistory[row].substring(numIndex +1, endOfNumber))
+
+      parsedHand.players.push(["player " + (parsedHand.players.length+1), chips]);
+      row++;
+    }
+
+    //advance to the hand row
+    while( handHistory[row].indexOf("*** ") !== 0 ){
+      row++;
+    }
+
+    // need to be prepared for hand history where no hand is known
+    var ownHand = handHistory[++row].split('[');
+    ownHand = ownHand[ownHand.length-1].substring(0, 5).split(" ");
+
+    parsedHand.hands.push(ownHand);
+
+    // todo: check if there are other hands available
+
+    //advance to the next event
+    while( handHistory[row].indexOf("*** ") !== 0 ){
+      parsedHand.preFlopActions.push(handHistory[row]);
+      row++;
+    }
+
+    //check if there's a FLOP
+    if(handHistory[row].indexOf("FLOP") !== -1){
+      //get the FLOP cards
+      parsedHand.flop = handHistory[row].split('[');
+      parsedHand.flop = parsedHand.flop[1].substring(0, 8).split(" ");
+      parsedHand.table = parsedHand.flop.slice();
+      row++;
+
+      // advance to the next event
+      while( handHistory[row].indexOf("*** ") !== 0 ){
+        parsedHand.flopActions.push(handHistory[row]);
+        row++;
+      }
+
+      // check if there's a TURN
+      if( handHistory[row].indexOf("TURN") === 4){
+        //get the TURN card
+        parsedHand.turn = handHistory[row].split('[');
+        parsedHand.turn = parsedHand.turn[2].substring(0, 2);
+        parsedHand.table.push(parsedHand.turn);
+        row++;
+
+
+        // advance to the next event
+        while( handHistory[row].indexOf("*** ") !== 0 ){
+          parsedHand.turnActions.push(handHistory[row]);
+          row++;
+        }
+
+        // check if there's a RIVER
+        if( handHistory[row].indexOf("RIVER") === 4){
+          //get the RIVER card
+          parsedHand.river = handHistory[row].split('[');
+          parsedHand.river = parsedHand.river[2].substring(0, 2);
+          parsedHand.table.push(parsedHand.river);
+          row++;
+
+          // advance to the next event
+          while( handHistory[row].indexOf("*** ") !== 0 ){
+            parsedHand.riverActions.push(handHistory[row]);
+            row++;
+          }
+
         }
 
       }
 
     }
-
   }
   return parsedHand;
 }
--- testHands.js
+++ testHands.js
@@ -2,70 +2,153 @@ exports.testHands = [];
 
 // pokerstars, holdem, tournament, 6 players, portuguese, flop, hand available
 exports.testHands.push("Mão PokerStars #137737997245: Torneio #1270291825, 4500+500 Hold'em No Limit - Nível I (10/20) - 2015/07/07 16:42:52 ET \n" +
-"Mesa '1270291825 1' 6-max Lugar #1 é o botão \n" +
-"Lugar 1: iagowp (500 em fichas) \n" +
-"Lugar 2: JFDX7WC (500 em fichas) \n" +
-"Lugar 3: paluik (500 em fichas) \n" +
-"Lugar 4: INDIA598 (500 em fichas) está sit out \n" +
-"Lugar 5: JACOPO70 (500 em fichas) \n" +
-"Lugar 6: skat=hinni1 (500, em fichas) \n" +
-"iagowp: coloca ante 2 \n" +
-"JFDX7WC: coloca ante 2 \n" +
-"paluik: coloca ante 2 \n" +
-"INDIA598: coloca ante 2 \n" +
-"JACOPO70: coloca ante 2 \n" +
-"skat=hinni1: coloca ante 2 \n" +
-"JFDX7WC: paga o small blind 10 \n" +
-"paluik: paga o big blind 20 \n" +
-"*** CARTAS DA MÃO *** \n" +
-"iagowp recebe [Jc 9s] \n" +
-"INDIA598: desiste  \n" +
-"JACOPO70: desiste  \n" +
-"skat=hinni1: desiste  \n" +
-"iagowp: iguala 20 \n" +
-"JFDX7WC: aumenta 20 para 40 \n" +
-"paluik: desiste  \n" +
-"iagowp: iguala 20 \n" +
-"*** FLOP *** [8c 2d 6s] \n" +
-"JFDX7WC: aposta 100 \n" +
-"iagowp: aumenta 100 para 200 \n" +
-"JFDX7WC: aumenta 258 para 458 e está all-in \n" +
-"iagowp: desiste  \n" +
-"Aposta não-igualada (258) voltou para JFDX7WC \n" +
-"JFDX7WC recebeu 512 do pote \n" +
-"*** SUMÁRIO *** \n" +
-"Total pote 512 | comissão 0  \n" +
-"Mesa [8c 2d 6s] \n" +
-"Lugar 1: iagowp (Botão) desistiu no Flop \n" +
-"Lugar 2: JFDX7WC (small blind) recebeu (512) \n" +
-"Lugar 3: paluik (big blind) desistiu antes Flop \n" +
-"Lugar 4: INDIA598 desistiu antes Flop (não apostou) \n" +
-"Lugar 5: JACOPO70 desistiu antes Flop (não apostou) \n" +
-"Lugar 6: skat=hinni1 desistiu antes Flop (não apostou)");
+	"Mesa '1270291825 1' 6-max Lugar #1 é o botão \n" +
+	"Lugar 1: iagowp (500 em fichas) \n" +
+	"Lugar 2: JFDX7WC (500 em fichas) \n" +
+	"Lugar 3: paluik (500 em fichas) \n" +
+	"Lugar 4: INDIA598 (500 em fichas) está sit out \n" +
+	"Lugar 5: JACOPO70 (500 em fichas) \n" +
+	"Lugar 6: skat=hinni1 (500, em fichas) \n" +
+	"iagowp: coloca ante 2 \n" +
+	"JFDX7WC: coloca ante 2 \n" +
+	"paluik: coloca ante 2 \n" +
+	"INDIA598: coloca ante 2 \n" +
+	"JACOPO70: coloca ante 2 \n" +
+	"skat=hinni1: coloca ante 2 \n" +
+	"JFDX7WC: paga o small blind 10 \n" +
+	"paluik: paga o big blind 20 \n" +
+	"*** CARTAS DA MÃO *** \n" +
+	"iagowp recebe [Jc 9s] \n" +
+	"INDIA598: desiste  \n" +
+	"JACOPO70: desiste  \n" +
+	"skat=hinni1: desiste  \n" +
+	"iagowp: iguala 20 \n" +
+	"JFDX7WC: aumenta 20 para 40 \n" +
+	"paluik: desiste  \n" +
+	"iagowp: iguala 20 \n" +
+	"*** FLOP *** [8c 2d 6s] \n" +
+	"JFDX7WC: aposta 100 \n" +
+	"iagowp: aumenta 100 para 200 \n" +
+	"JFDX7WC: aumenta 258 para 458 e está all-in \n" +
+	"iagowp: desiste  \n" +
+	"Aposta não-igualada (258) voltou para JFDX7WC \n" +
+	"JFDX7WC recebeu 512 do pote \n" +
+	"*** SUMÁRIO *** \n" +
+	"Total pote 512 | comissão 0  \n" +
+	"Mesa [8c 2d 6s] \n" +
+	"Lugar 1: iagowp (Botão) desistiu no Flop \n" +
+	"Lugar 2: JFDX7WC (small blind) recebeu (512) \n" +
+	"Lugar 3: paluik (big blind) desistiu antes Flop \n" +
+	"Lugar 4: INDIA598 desistiu antes Flop (não apostou) \n" +
+	"Lugar 5: JACOPO70 desistiu antes Flop (não apostou) \n" +
+	"Lugar 6: skat=hinni1 desistiu antes Flop (não apostou)");
 
 // pokerstars, holdem, tournament, 2 players, portuguese, turn, no-hand available
 exports.testHands.push("Mão PokerStars #137738351488: Torneio #1270291825, 4500+500 Hold'em No Limit - Nível IV (30/60) - 2015/07/07 16:49:31 ET  \n" +
-"Mesa '1270291825 1' 6-max Lugar #2 é o botão \n" +
-"Lugar 2: JFDX7WC (1140 em fichas)  \n" +
-"Lugar 5: JACOPO70 (1860 em fichas)  \n" +
-"JFDX7WC: coloca ante 6 \n" +
-"JACOPO70: coloca ante 6 \n" +
-"JFDX7WC: paga o small blind 30 \n" +
-"JACOPO70: paga o big blind 60 \n" +
-"*** CARTAS DA MÃO *** \n" +
-"JFDX7WC: iguala 30 \n" +
-"JACOPO70: passa  \n" +
-"*** FLOP *** [Qs 3c Kc] \n" +
-"JACOPO70: passa  \n" +
-"JFDX7WC: passa  \n" +
-"*** TURN *** [Qs 3c Kc] [Ac] \n" +
-"JACOPO70: passa  \n" +
-"JFDX7WC: aposta 60 \n" +
-"JACOPO70: desiste  \n" +
-"Aposta não-igualada (60) voltou para JFDX7WC \n" +
-"JFDX7WC recebeu 132 do pote \n" +
-"*** SUMÁRIO *** \n" +
-"Total pote 132 | comissão 0  \n" +
-"Mesa [Qs 3c Kc Ac] \n" +
-"Lugar 2: JFDX7WC (Botão) (small blind) recebeu (132) \n" +
-"Lugar 5: JACOPO70 (big blind) desistiu no Turn \n");
+	"Mesa '1270291825 1' 6-max Lugar #2 é o botão \n" +
+	"Lugar 2: JFDX7WC (1140 em fichas)  \n" +
+	"Lugar 5: JACOPO70 (1860 em fichas)  \n" +
+	"JFDX7WC: coloca ante 6 \n" +
+	"JACOPO70: coloca ante 6 \n" +
+	"JFDX7WC: paga o small blind 30 \n" +
+	"JACOPO70: paga o big blind 60 \n" +
+	"*** CARTAS DA MÃO *** \n" +
+	"JFDX7WC: iguala 30 \n" +
+	"JACOPO70: passa  \n" +
+	"*** FLOP *** [Qs 3c Kc] \n" +
+	"JACOPO70: passa  \n" +
+	"JFDX7WC: passa  \n" +
+	"*** TURN *** [Qs 3c Kc] [Ac] \n" +
+	"JACOPO70: passa  \n" +
+	"JFDX7WC: aposta 60 \n" +
+	"JACOPO70: desiste  \n" +
+	"Aposta não-igualada (60) voltou para JFDX7WC \n" +
+	"JFDX7WC recebeu 132 do pote \n" +
+	"*** SUMÁRIO *** \n" +
+	"Total pote 132 | comissão 0  \n" +
+	"Mesa [Qs 3c Kc Ac] \n" +
+	"Lugar 2: JFDX7WC (Botão) (small blind) recebeu (132) \n" +
+	"Lugar 5: JACOPO70 (big blind) desistiu no Turn \n");
+
+// pokerstars, holden, cash, 4 players, english, river, hand available
+exports.testHands.push("PokerStars Zoom Hand #139357955742:  Hold'em No Limit ($2.50/$5.00) - 2015/08/14 3:26:04 ET \n" +
+"Table 'Lambda Velorum' 6-max Seat #1 is the button\n" +
+"Seat 1: ArtPlay ($574.70 in chips) \n" +
+"Seat 2: sphinmx ($623.48 in chips) \n" +
+"Seat 3: Melodyqoo ($611.85 in chips) \n" +
+"Seat 4: zerfer03 ($517.50 in chips) \n" +
+"sphinmx: posts small blind $2.50\n" +
+"Melodyqoo: posts big blind $5\n" +
+" HOLE CARDS \n" +
+"Dealt to sphinmx [Ad Ts]\n" +
+"zerfer03: folds\n" +
+"ArtPlay: folds \n" +
+"sphinmx: raises $10 to $15\n" +
+"Melodyqoo: raises $30 to $45\n" +
+"sphinmx: calls $30\n" +
+" FLOP  [Tc 8d Td]\n" +
+"sphinmx: checks\n" +
+"Melodyqoo: bets $45\n" +
+"sphinmx: raises $75 to $120\n" +
+"Melodyqoo: calls $75\n" +
+" TURN  [Tc 8d Td] [Js]\n" +
+"sphinmx: bets $458.48 and is all-in\n" +
+"Melodyqoo: calls $446.85 and is all-in\n" +
+"Uncalled bet ($11.63) returned to sphinmx\n" +
+" RIVER  [Tc 8d Td Js] [Qs]\n" +
+" SHOW DOWN \n" +
+"sphinmx: shows [Ad Ts] (three of a kind, Tens)\n" +
+"Melodyqoo: shows [Jc Jh] (a full house, Jacks full of Tens) \n" +
+"Melodyqoo collected $1222.20 from pot \n" +
+" SUMMARY \n" +
+"Total pot $1223.70 | Rake $1.50 \n" +
+"Board [Tc 8d Td Js Qs] \n" +
+"Seat 1: ArtPlay (button) folded before Flop (didn't bet)\n" +
+"Seat 2: sphinmx (small blind) showed [Ad Ts] and lost with three of a kind, Tens \n" +
+"Seat 3: Melodyqoo (big blind) showed [Jc Jh] and won ($1222.20) with a full house, Jacks full of Tens \n" +
+"Seat 4: zerfer03 folded before Flop (didn't bet)");
+
+// pokerstars, holden, tournament, 9 players, english, no flop, hand available
+exports.testHands.push("PokerStars Hand #129546885146: Tournament #1118959248, $10+$1 USD Hold'em No Limit - Level I (10/20) - 2015/01/29 12:15:12 ET\n" +
+"Table '1118959248 5' 9-max Seat #1 is the button\n" +
+"Hinrekas re-buys and receives 3000 chips for $10.00\n" +
+"eardleym re-buys and receives 3000 chips for $10.00\n" +
+"Llkee re-buys and receives 3000 chips for $10.00\n" +
+"b.serafa re-buys and receives 3000 chips for $10.00\n" +
+"konnh4nd re-buys and receives 3000 chips for $10.00\n" +
+"Seat 1: MaximSG (3000 in chips) \n" +
+"Seat 2: Llkee (6000 in chips) \n" +
+"Seat 3: b.serafa (6000 in chips) \n" +
+"Seat 4: Hinrekas (6000 in chips) \n" +
+"Seat 5: eardleym (6000 in chips) \n" +
+"Seat 6: konnh4nd (6000 in chips) \n" +
+"Seat 7: Moeses87 (3000 in chips) \n" +
+"Seat 8: ruslanmd (3000 in chips) \n" +
+"Seat 9: jordanblg (3000 in chips) \n" +
+"Llkee: posts small blind 10\n" +
+"b.serafa: posts big blind 20\n" +
+"*** HOLE CARDS ***\n" +
+"Dealt to b.serafa [Ks 7c]\n" +
+"Hinrekas: folds \n" +
+"eardleym: folds \n" +
+"konnh4nd: folds \n" +
+"Moeses87: raises 20 to 40\n" +
+"ruslanmd: folds \n" +
+"jordanblg: folds \n" +
+"MaximSG: folds \n" +
+"Llkee: folds \n" +
+"b.serafa: folds \n" +
+"Uncalled bet (20) returned to Moeses87\n" +
+"Moeses87 collected 50 from pot\n" +
+"Moeses87: doesn't show hand \n" +
+"*** SUMMARY ***\n" +
+"Total pot 50 | Rake 0 \n" +
+"Seat 1: MaximSG (button) folded before Flop (didn't bet)\n" +
+"Seat 2: Llkee (small blind) folded before Flop\n" +
+"Seat 3: b.serafa (big blind) folded before Flop\n" +
+"Seat 4: Hinrekas folded before Flop (didn't bet)\n" +
+"Seat 5: eardleym folded before Flop (didn't bet)\n" +
+"Seat 6: konnh4nd folded before Flop (didn't bet)\n" +
+"Seat 7: Moeses87 collected (50)\n" +
+"Seat 8: ruslanmd folded before Flop (didn't bet) \n" +
+"Seat 9: jordanblg folded before Flop (didn't bet)");
